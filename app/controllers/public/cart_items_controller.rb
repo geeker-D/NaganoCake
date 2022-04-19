dc92b3c431acdac7c29fbaf76e87c23584cf9a26
kkazu-hoge <928kuni@gmail.com>
2022-04-19 06:59:07 +0000

    [update]cart_item view setup

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,6 +3,11 @@ class Public::CartItemsController < Public::ApplicationController
     # binding.pry
     customer = current_customer
     @cart_items = customer.cart_items
+    #合計金額(送料なし、税込み)
+    @total_payment_no_shipfee = 0
+    @cart_items.each do |cart_item|
+      @total_payment_no_shipfee += (cart_item.item.price_non_tax * 1.1).floor * cart_item.amount
+    end
 
   end
 
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,5 +2,10 @@ class CartItem < ApplicationRecord
 
   belongs_to :customer
   belongs_to :item
+  
+  
+  def total_payment_no_shipfee
+    
+  end
 
 end
