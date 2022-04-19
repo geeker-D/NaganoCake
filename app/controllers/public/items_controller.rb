c7eff03988430eec6c8fc51c39c25579cea6c082
ksyrba <ksyrbaba0812@gmail.com>
2022-04-19 01:20:58 +0000

    [add]item_index

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,16 @@
 class Public::ItemsController < Public::ApplicationController
   def index
+    @items = Item.page(params[:page])
+    @genres = Genre.all
   end
 
   def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
+  end
+  
+  private
+  def item_params
+    params.require(:item).permit(:name, :summary, :price_non_tax, :is_active, :image)  
   end
 end
