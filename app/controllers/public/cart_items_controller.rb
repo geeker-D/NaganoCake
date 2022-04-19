2ce0b0cb49b21fba8b364708ad763abf6977e8c9
kkazu-hoge <928kuni@gmail.com>
2022-04-19 08:59:04 +0000

    [update]cart_items controller bag fix

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -15,7 +15,7 @@ class Public::CartItemsController < Public::ApplicationController
 
   def update
     @cart_item = CartItem.find(params[:id])
-    if @cart_item.update(cart_item_params)
+    if @cart_item.update(cart_item_update_params)
       redirect_to request.referer, notice: "カート内の商品(#{@cart_item.item.name})の数量を変更しました。"
     else
       # render "index"
@@ -24,20 +24,24 @@ class Public::CartItemsController < Public::ApplicationController
 
   def destroy
     cart_item = CartItem.find(params[:id])
-    cart_item.destroy
-    redirect_to request.referer, notice: "カート内の商品を1件(#{cart_item.item.name})削除しました。"
+    if cart_item.destroy
+      redirect_to request.referer, notice: "カート内の商品を1件(#{cart_item.item.name})削除しました。"
+    else
+       # render "index"
+    end
   end
 
   def destroy_all
     @cart_items = current_customer.cart_items
+    # render "index" if !@cart_items
     @cart_items.each do |cart_item|
       cart_item.destroy
     end
-    redirect_to request.referer, notice: "カート内の商品は全て削除されました。"
+      redirect_to request.referer, notice: "カート内の商品は全て削除されました。"
   end
 
   private
-  def cart_item_params
+  def cart_item_update_params
     params.require(:cart_item).permit(:amount)
   end
 
