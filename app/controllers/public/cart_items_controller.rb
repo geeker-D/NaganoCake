48487ea9884653849a98dac041fb403cf3c09e8b
kkazu-hoge <928kuni@gmail.com>
2022-04-19 16:14:10 +0000

    [update]cart_item model logic

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,13 +1,8 @@
 class Public::CartItemsController < Public::ApplicationController
+
   def index
     @cart_items = current_customer.cart_items
-    #合計金額(送料なし、税込み)
-    @total_payment_no_shipfee = 0
-    @cart_items.each do |cart_item|
-      @total_payment_no_shipfee += (cart_item.item.price_non_tax * 1.1).floor * cart_item.amount
-    end
-
-    # @total_payment_no_shipfee = customer.items.total_payment_no_shipfee
+    @total_payment_no_shipfee = CartItem.total_payment_no_shipfee(@cart_items)
   end
 
   def create
@@ -18,7 +13,9 @@ class Public::CartItemsController < Public::ApplicationController
     if @cart_item.update(cart_item_update_params)
       redirect_to request.referer, notice: "カート内の商品(#{@cart_item.item.name})の数量を変更しました。"
     else
-      # render "index"
+      @cart_items = current_customer.cart_items
+      @total_payment_no_shipfee = CartItem.total_payment_no_shipfee(@cart_items)
+      render "index"
     end
   end
 
@@ -27,17 +24,18 @@ class Public::CartItemsController < Public::ApplicationController
     if cart_item.destroy
       redirect_to request.referer, notice: "カート内の商品を1件(#{cart_item.item.name})削除しました。"
     else
-       # render "index"
+      @cart_items = current_customer.cart_items
+      @total_payment_no_shipfee = CartItem.total_payment_no_shipfee(@cart_items)
+      render "index"
     end
   end
 
   def destroy_all
     @cart_items = current_customer.cart_items
-    # render "index" if !@cart_items
     @cart_items.each do |cart_item|
       cart_item.destroy
     end
-      redirect_to request.referer, notice: "カート内の商品は全て削除されました。"
+    redirect_to request.referer, notice: "カート内の商品は全て削除されました。"
   end
 
   private
