4e7b294b2dda42f1fb4b324beb3f2fd2e2398314
kkazu-hoge <928kuni@gmail.com>
2022-04-20 08:12:40 +0000

    [update]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,13 +6,18 @@ class Public::OrdersController < Public::ApplicationController
   end
 
   def new
-    
+    @order = Order.new
+    @current_customer_post = current_customer.post_code
+    @current_customer_address = current_customer.address
+    @current_customer_to_name = "#{current_customer.last_name}" + "#{current_customer.first_name}"
+
   end
 
   def create
   end
 
   def order_preconfirm
+
   end
 
   def complete
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,7 +3,8 @@ class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
 
-
+  validates :amount, numericality: {greater_than: 0,less_than: 11}
+  
   def self.total_payment_no_shipfee(cart_items)
     total_payment_no_shipfee = 0
 
