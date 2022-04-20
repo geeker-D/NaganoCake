d43325544c5674aea5a1812047aedbd2cac30ee2
kkazu-hoge <928kuni@gmail.com>
2022-04-20 12:36:27 +0000

    [update]order_input finish

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,17 +7,37 @@ class Public::OrdersController < Public::ApplicationController
 
   def new
     @order = Order.new
-    @current_customer_post = current_customer.post_code
-    @current_customer_address = current_customer.address
-    @current_customer_to_name = "#{current_customer.last_name}" + "#{current_customer.first_name}"
+    @shipping_info = ShippingAddress.where(customer_id: current_customer.id).map{ |sa| ["〒"+sa.post_code+" "+sa.address+" "+sa.to_name, sa.id]}
 
   end
 
   def create
+
   end
 
   def order_preconfirm
+    # binding.pry
+    # address_radio_type_customerMT = 1
+    # address_radio_type_shippingMT = 2
+    # address_radio_type_newInput = 3
 
+    # case params[:order][:address_radio_type]
+    # when address_radio_type_customerMT then
+    #   @payment_type = a
+    #   # ログイン顧客の住所情報を結合して格納する
+    #   @shipping_address = b
+    # when address_radio_type_shippingMT
+    #   @payment_type = a
+    #   # セレクトボックスの配送先IDを元に検索し値を格納する
+    #   @shipping_address = b
+    # when address_radio_type_newInput
+    #   @payment_type = a
+    #   # 送信パラメータの入力内容を結合して配送先住所を設定する
+    #   @shipping_address = b
+    # else
+    #   #エラーメッセージを返す
+    # end
+    # render "show",{ customer: @customer, url: order_path(@customer.id) }
   end
 
   def complete
