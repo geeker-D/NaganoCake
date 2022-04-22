e5a020968fd5e1fbb23786abf9368f45d2e81630
shinju0514 <shinju0514yellow@gmail.com>
2022-04-22 14:25:28 +0000

    [update]order_detail

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,7 +1,6 @@
 class Admin::OrderDetailsController < Admin::ApplicationController
 
   def update
-    @order = Order.find(params[:order_id])
     @order_detail = OrderDetail.find(params[:id])
     @order_detail.update(order_detail_params)
     redirect_back(fallback_location: root_path)
