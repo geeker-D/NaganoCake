208ec049dc6c82f05d27706e294609ddfddb4844
kkazu-hoge <928kuni@gmail.com>
2022-04-23 12:45:17 +0000

    [update]admin header search function available

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,4 +1,7 @@
 class Admin::SearchesController < Admin::ApplicationController
   def search_item
+    @item_name = params[:search_text]
+    @genres = Genre.page(params[:page])
+    @search_result = Item.search_item_fnc(@item_name)
   end
 end
