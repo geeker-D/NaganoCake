def44e3e930af2a51e0636baf07ed47f1b769e3b
kkazu-hoge <928kuni@gmail.com>
2022-04-23 12:35:22 +0000

    [update]header search function available

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,6 +1,8 @@
 class Public::SearchesController < Public::ApplicationController
   def search_item
-    
+    @item_name = params[:search_text]
+    @genres = Genre.page(params[:page])
+    @search_result = Item.search_item_fnc(@item_name)
   end
 
   def search_genre
