74d95e2cb9e831637bea1be7cc26115c17d7542c
kkazu-hoge <928kuni@gmail.com>
2022-04-23 01:50:24 +0000

    [update]bag fix

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,7 +1,10 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :customer
-  
+
   validates :post_code, {length: {is: 7} }
   validates :address, :to_name, presence: true
-  
+
+   #スコープ
+   scope :customer_relation, -> (current_customer) {where(customer_id: current_customer.id).map{ |sa| ["〒"+sa.post_code+" "+sa.address+" "+sa.to_name, sa.id]}}
+
 end
