4ee823b7880353c84fe211049704180f41619d24
shinju0514 <shinju0514yellow@gmail.com>
2022-04-25 09:57:58 +0000

    [update]layout

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -10,6 +10,7 @@ class Admin::OrdersController < Admin::ApplicationController
       @order_details = @order.order_details
       @order.update(order_params)
       flash[:notice] = "更新しました"
+      # 注文ステータスが入金確認へ変更されると、製作ステータスがupdate.allによって製作ステータスが全て製作待ちへ変更される。
     if @order.status == "confirm_deposit"
       @order_details.update_all(product_status: "wait_making")
       redirect_to admin_order_path(@order)
